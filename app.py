67299e6414aaf0e55cfe2e7556134ea344045c58
Ola1884 <ola4.eng@gmail.com>
2026-03-07 00:37:04 +0200

    Day 2: Added multi-page structure and course input

--- app.py
+++ app.py
@@ -2,8 +2,14 @@ import streamlit as st
 
 st.set_page_config(page_title="AI Study Planner", page_icon="🎓")
 
-st.title("🎓 AI Study Planner")
-st.write("Welcome! Please set up your courses first.")
+st.title("_وتيرة_ :bar_chart:",width="stretch",text_alignment="right")
+st.write("ابدأ تنتهي")
+st.write("خطط اجتهد تصل")
 
 st.page_link("pages/Tasks.py", label="Add Tasks", icon="📝")
-#st.page_link("pages/2_🗓️_My_Plan.py", label="🗓️ View & Generate Plan", icon="🗓️")
+st.page_link("pages/Plan.py", label="View & Generate Plan", icon="🗓️")
+if st.button(label="Tasks",on_click="pages/Tasks.py"):
+    st.switch_page("pages/Tasks.py", label="Add Tasks", icon="📝")
+with st.sidebar:
+    st.image("", width=50)
+    st.title("Menu")
